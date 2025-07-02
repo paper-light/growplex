f4b0fb0341bd0f684eaea91add654a2d538816d8
plw <kwazzartcreature@gmail.com>
2025-07-02 08:31:03 -0700

    rough crawl4ai test

--- src/server/init.ts
+++ src/server/init.ts
@@ -4,10 +4,15 @@ import http from "http";
 import { attachSocketIO } from "./chat/controller";
 import { pbAdminMiddleware } from "./auth/middleware";
 
+import { submitCrawl } from "../lib/crawl4ai/service";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 export async function main(astroHandler: any) {
+  const res = await submitCrawl(["https://www.google.com"]);
+  console.log(res);
+
   app.use(pbAdminMiddleware);
   app.use(express.static("dist/client"));
   app.use(astroHandler);
